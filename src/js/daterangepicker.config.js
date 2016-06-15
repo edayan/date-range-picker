9805b405fac45d1144cb9be532ae09809cfdb081
edayan <saju.edayan@gmail.com>
2016-06-16 00:53:55 +0530

    final code after minification

---
+++ src/js/daterangepicker.config.js
@@ -0,0 +1,103 @@
+$(function() {
+    var selectedStartDate,
+        selectedDate,
+        dayInSelectedStartDate;
+
+    function startStartDatePicker() {
+        $('#start-datetimepicker').dateRangePicker({
+            time: {
+                enabled: true
+            },
+            singleMonth: true,
+            singleDate: true,
+            startDate: false,
+            endDate: false,
+            endDateField: false,
+            format: 'DD.MM.YYYY HH:mm',
+            autoClose: false,
+            tableId: 'start-date-table',
+            getSelectedEndDate: function() {
+                return ($('#date-time-fixed').data('end'));
+            },
+            setDateToDom: function(time) {
+                $('#date-time-fixed').data('start', time);
+                return;
+            },
+            setValue: function(s, s1, s2) {
+                $('#start-date-range').val(s1);
+            },
+            showShortcuts: false,
+            beforeShowDay: function(t) {
+                var yesterday = new Date().setDate(new Date().getDate() - 1);
+                var valid = !((t <= yesterday) ||
+                    (this.getSelectedEndDate() ? t > this.getSelectedEndDate() : false));
+                var _class = '';
+                var _tooltip = valid ? '' : (t <= yesterday) ? 'Cannot select previous dates' : 'Arrival date cannot be after departure date';
+                return [valid, _class, _tooltip];
+            }
+        }, this)
+        .bind('datepicker-first-date-selected', function(event, obj) {
+            event.stopPropagation();
+            selectedDate = obj.date1;
+            selectedStartDate = getFormattedDate(selectedDate);
+            selectedTime = getFormatedTime(selectedDate);
+            $('#start-date-range').val(selectedStartDate + " " + selectedTime);
+        })
+        ;
+    };
+
+    function getFormattedDate(todayTime) {
+        var month = todayTime.getMonth() + 1;
+        var day = todayTime.getDate();
+        var year = todayTime.getFullYear();
+        return day + "." + month + "." + year;
+    };
+
+    function getFormatedTime(todayTime) {
+        var hours = todayTime.getHours();
+        var minutes = todayTime.getMinutes();
+        return hours + ":" + minutes;
+    };
+
+    function startEndDatePicker() {
+        $('#end-datetimepicker').dateRangePicker({
+            time: {
+                enabled: true
+            },
+            singleMonth: true,
+            singleDate: true,
+            endDate: false,
+            format: 'DD.MM.YYYY HH:mm',
+            autoClose: false,
+            endDateField: true,
+            tableId: 'end-date-table',
+            askToSelectStartDateFirst: function() {
+                alert('Please select arrival date first');
+            },
+            getSelectedStartDate: function() {
+                return ($('#date-time-fixed').data('start'));
+            },
+            setDateToDom: function(time) {
+                $('#date-time-fixed').data('end', time);
+                return;
+            },
+            setValue: function(s, s1, s2) {
+                $('#end-date-range').val(s2);
+            },
+            showShortcuts: false,
+            beforeShowDay: function(t) {
+                if (this.getSelectedStartDate()) {
+                    var valid = !(t < this.getSelectedStartDate());
+                    var _class = '';
+                    var _tooltip = valid ? '' : 'Departure date cannot be before arrival date';
+                    return [valid, _class, _tooltip];
+                }
+                return [true, _class, _tooltip];
+            }
+        }, this);
+    };
+
+    startStartDatePicker();
+    startEndDatePicker();
+
+});
